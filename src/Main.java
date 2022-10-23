453c1488be5cb581e0a3178cb445b759933f7de6
Vyrvins <olya.vyrvinskaya@gmail.com>
2022-10-23 16:02:48 +0300

    modified main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 100000; // сумма начального счета
-        int b = 500; // сумма пополнения
-        int bonus = (a + b) / 100;
-        if (a + b >= 1100) {
+        int a = 100; // сумма начального счета
+        int b = 1100; // сумма пополнения
+        int bonus = (b) / 100;
+        if (b>= 1000) {
             System.out.println("Cумма бонусов " + bonus);
         } else {
             System.out.println("Cумма бонусов " + 0);
         }
-        System.out.println("Итоговая сумма на счете " + (a + b));
+        System.out.println("Итоговая сумма на счете " + (a + b + bonus));
 
     }
 }
